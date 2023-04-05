0f649eed5260f86fab889e499918abf012a3bbd9
josh4got <125082025+josh4got@users.noreply.github.com>
2023-04-04 20:41:31 -0600

    added function to append cocktail if local storag has cocktail key

--- assets/js/BIGBOISCRIPTING.js
+++ assets/js/BIGBOISCRIPTING.js
@@ -1,12 +1,75 @@
-let cocktailButtons = document.getElementsByClassName("btnC");
-let cocktail = "";
-for (let i = 0; i < cocktailButtons.length; i++) {
-  let cocktailButton = cocktailButtons[i];
-  cocktailButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    cocktail = this.textContent;
-    console.log(cocktail);
-    if (cocktail === "Yes") console.log("Happy Hour");
-    else console.log("No Happy Hour");
-  });
+let newcocktail = [];
+
+function getcocktail(event) {
+  event.preventDefault();
+  let newEL = event.target.id;
+  localStorage.setItem("cocktail", newEL);
+  console.log(localStorage);
+  console.log(newEL);
 }
+
+function getIngredients(recipe) {
+  const ingredients = [];
+  for (let i = 1; i <= 15; i++) {
+    const ingredient = recipe[`strIngredient${i}`];
+    const measure = recipe[`strMeasure${i}`];
+    if (ingredient && measure) {
+      ingredients.push(`<li>${measure} ${ingredient}</li>`);
+    } else if (ingredient) {
+      ingredients.push(`<li>${ingredient}</li>`);
+    }
+  }
+  return ingredients.join("");
+}
+
+function appendRandomCocktailRecipe() {
+  const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then(({ drinks: [recipe] }) => {
+      const ingredients = getIngredients(recipe);
+      const recipeHtml = `
+        <div class="card h-24 vw-full flex m-5 p-3">
+          <h2 class="card-title">${recipe.strDrink}</h2>
+          <img src="${recipe.strDrinkThumb}" alt="${recipe.strDrink}" class="card-img">
+          <div class="card-body">
+            <h3 class="card-subtitle">Ingredients:</h3>
+            <ul class="card-list">${ingredients}</ul>
+            <h3 class="card-subtitle">Instructions:</h3>
+            <p class="card-text">${recipe.strInstructions}</p>
+          </div>
+        </div>
+      `;
+      document
+        .querySelector("#recipes")
+        .appendChild(
+          document.createRange().createContextualFragment(recipeHtml)
+        );
+    });
+}
+
+function setPageActions() {
+  let page = document.body.id;
+  switch (page) {
+    case "Welcome":
+      // Perform actions for the index page
+      let cocktail = document.querySelector("#yes");
+      cocktail.addEventListener("click", getcocktail);
+      console.log("You are on the index page.");
+      break;
+    case "calendar":
+      // Perform actions for the calendar page
+      let storedCocktail = localStorage.getItem("cocktail");
+      console.log(storedCocktail);
+      if (storedCocktail && storedCocktail.length > 0) {
+        appendRandomCocktailRecipe();
+      }
+      console.log("You are on the calendar page.");
+      break;
+    default:
+      console.log("Unknown page.");
+  }
+}
+
+window.addEventListener("load", setPageActions);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,23 @@
+var apiKey = "b48a85cecf0a47eba4b20401a0ee0e14";
+var button = document.getElementById("btn");
 
-var apiKey = 'b48a85cecf0a47eba4b20401a0ee0e14';
-var button = document.getElementById('btn');
+function generateRecipe(event) {
+  console.log("Hello");
+  event.preventDefault();
+  var urlRequest = `https://api.spoonacular.com/recipes/random?number=1&type=main-course&apiKey=${apiKey}`;
 
-
-function generateRecipe(event){
-    console.log("Hello");
-    event.preventDefault();
-    var urlRequest = `https://api.spoonacular.com/recipes/random?number=1&type=main-course&apiKey=${apiKey}`;
-
-    fetch(urlRequest,{
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  fetch(urlRequest, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-      .then(function (response) {
-        return response.json();
-      })
-        
-      .then(function (data) {
-        console.log(data);
-      });
-        
-      
-      }
-    
-button.addEventListener('click', generateRecipe);
+
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+// button.addEventListener('click', generateRecipe);
