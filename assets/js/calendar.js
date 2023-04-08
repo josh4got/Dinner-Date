4357e1cf7dd967f33efb8850248db47a9b6c8051
Phil Dangerfield <philipdangerfield@Philips-MBP.askey.com>
2023-04-08 11:43:24 -0600

    adjusted the calendar javascript to create a card and append recipe to the card as well as added styling

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,19 +1,24 @@
 var apiKey = '1ba08610419e4c7a9791d166c28d523e';
 var now = dayjs();
+var button = document.getElementById('yes');
 console.log(now);
+
+var generateBtn = document.getElementById('generate');
 
 
 //var recipeName = recipeName.innerText
 // recipeLink = recipeName.href
-var urlRequest = `https://api.spoonacular.com/recipes/random?number=3&tags=dinner&apiKey=${apiKey}`;
-var recipeObject = ''
+var urlRequest = `https://api.spoonacular.com/recipes/random?number=1&tags=dinner&apiKey=${apiKey}`;
+var recipeObject = '';
 
-generateRecipe(urlRequest);
+// generateRecipe(urlRequest);
+
+
 
 function generateRecipe(urlRequest){
   fetch(urlRequest,{
       headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
       }
   })
   .then(function (response) {
@@ -27,38 +32,55 @@ function generateRecipe(urlRequest){
 }
 
 function displayRecipe(recipeObject) {
-  var recipeContainerEl = document.getElementById('recipes');
-
-  for (let i=0; i < recipeObject.recipes.length; i++){
-    var dates = document.createElement('p');
-    var recipeName = document.createElement('a');
-    var servings = document.createElement('p');
-    var prepTime = document.createElement('p');
-    var summary = document.createElement('p');
-    var calendarBtn = document.createElement('button');
-
-    recipeName.href = recipeObject.recipes[i].spoonacularSourceUrl;
-    recipeName.innerText = recipeObject.recipes[i].title;
-    var dtStamp = now.add(i, 'day').format('YYYYMMDD');
-    dates.setAttribute('data-dtStamp', dtStamp);
-    dates.innerText= now.add(i, 'day').format('dddd MMMM D');
-    summary.innerHTML = recipeObject.recipes[i].summary.split('.')[0] + '.';
-
-    prepTime.innerText = "Prep time: " + recipeObject.recipes[i].readyInMinutes + " minutes";
-    servings.innerText = "Serves: " + recipeObject.recipes[i].servings;
-
-    recipeContainerEl.appendChild(dates);
-    recipeContainerEl.appendChild(recipeName);
-    recipeContainerEl.appendChild(prepTime);
-    recipeContainerEl.appendChild(servings);
-    recipeContainerEl.appendChild(summary);
-    recipeContainerEl.appendChild(calendarBtn);
-
-    var calendarSummary = `Today's Recipe:${recipeName.innerText}`
-    var calendarDescription = recipeName.href;
-    var testIcs = 
-//!!Do not indent this template literal! Sorry it's ugly, but the .ics file type does not allow indentation and the template literal interprets indentation into the file.
-`BEGIN:VCALENDAR
+    
+    var recipeContainerEl = document.getElementById('recipes');
+    for (let i=0; i < recipeObject.recipes.length; i++){
+      // create a div for each recipe card
+      var cardEl = document.createElement('div');
+      cardEl.className = 'w-1/6 h-auto bg-gray-200 mx-1 px-5 flex flex-col gap-1 items-center mb-0 relative';
+      // create a div for the date and add a data attribute for dtStamp
+      var dateEl = document.createElement('p');
+      dateEl.className = 'text-lg font-bold mb-2';
+      var dtStamp = now.add(i, 'day').format('YYYYMMDD');
+      dateEl.setAttribute('data-dtStamp', dtStamp);
+      dateEl.innerText = now.add(i, 'day').format('dddd MMMM D');
+  
+      // create a div for the recipe name and link to the recipe page
+      var nameEl = document.createElement('h3');
+      nameEl.className = 'text-xl font-bold mt-2 mb-1 flex justify-center items-center';
+      var recipeLink = recipeObject.recipes[i].spoonacularSourceUrl;
+      var nameLink = document.createElement('a');
+      nameLink.href = recipeLink;
+      nameLink.innerText = recipeObject.recipes[i].title;
+      nameLink.target = '_blank';
+      nameEl.appendChild(nameLink);
+  
+      // create a div for the recipe summary
+      var summaryEl = document.createElement('p');
+      summaryEl.className = 'text-sm mb-2';
+      summaryEl.innerHTML = recipeObject.recipes[i].summary.split('.')[0] + '.';
+  
+      // create a div for the recipe prep time
+      var prepEl = document.createElement('p');
+      prepEl.className = 'text-sm mb-2';
+      prepEl.innerHTML = `Prep time: ${recipeObject.recipes[i].readyInMinutes} minutes`;
+  
+      // create a div for the recipe servings
+      var servingsEl = document.createElement('p');
+      servingsEl.className = 'text-sm mb-2 pb-2';
+      servingsEl.innerHTML = `Servings: ${recipeObject.recipes[i].servings}`;
+  
+      // create a button for adding to calendar
+      var calendarBtn = document.createElement('button');
+      calendarBtn.className = 'py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700 absolute bottom-0 mt-2';
+      calendarBtn.innerText = 'Add to calendar';
+  
+      // add event listener to button for adding to calendar
+      calendarBtn.addEventListener("click", function() {
+        var calendarSummary = `Today's recipe: ${nameLink.innerText}`;
+        var calendarDescription = recipeLink;
+var testIcs =` 
+BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//hacksw/handcal//NONSGML v1.0//EN
 BEGIN:VEVENT
@@ -70,19 +92,33 @@ DTEND:${dtStamp}T180000
 SUMMARY:${calendarSummary}
 DESCRIPTION:${calendarDescription}
 END:VEVENT
-END:VCALENDAR`
-    //This function creates an anchor with an href that generates an ics file. Code copied from Stack Overflow (https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server/18197341#18197341)	
-    function addToCalendar (filename, text) {
-      // var dtEnd = now.add(1, 'day').format('YYYYMMDD') // (i, 'day') add data attribute to i-th card
-      var calendarBtnHref = document.createElement('a');
-      calendarBtnHref.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-      calendarBtnHref.textContent = "Add to calendar";
-      calendarBtnHref.setAttribute('download', filename);
-      calendarBtn.appendChild(calendarBtnHref);
-      console.log(testIcs);
-    }
-    calendarBtn.addEventListener("click", addToCalendar("DinnerDate.ics", testIcs));
-  }
-}
+END:VCALENDAR`;
+  
+        var calendarBtnHref = document.createElement('a');
+        calendarBtnHref.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(testIcs));
+        calendarBtnHref.setAttribute('download', 'DinnerDate.ics');
+        calendarBtnHref.click();
+      });
+  
+      // append all elements to the recipe card div
+      cardEl.appendChild(dateEl);
+      cardEl.appendChild(nameEl);
+      cardEl.appendChild(summaryEl);
+      cardEl.appendChild(servingsEl);
+      cardEl.appendChild(prepEl);
+      cardEl.appendChild(calendarBtn);
 
-var dtStamp = now.format('YYYYMMDD');
+      recipeContainerEl.appendChild(cardEl);
+    }
+}
+      var dtStamp = now.format('YYYYMMDD');
+
+    //   button.addEventListener('click', function() {
+    //     generateRecipe(urlRequest);
+    //   });
+
+      generateBtn.addEventListener('click', function() {
+        window.location.href = 'calendar.html';
+      });
+
+
